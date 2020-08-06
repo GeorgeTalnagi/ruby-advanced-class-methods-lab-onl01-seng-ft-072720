f61e7dcb3957d183373e9751afcd9d320f26bc3e
George Talnagi <gtalnagi@yahoo.com>
2020-08-06 20:49:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,42 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name, artist_name)
+    @name = name 
+    @artist_name = artist_name
+    @@all << self 
+  end 
+  
+  
+  def self.new_by_name
+    @@all 
+  end 
+  
+  
+  def self.find_or_create_by_name
+  end 
+  
+ 
+  def self.alphabetical 
+  end 
+  
+  
+  def self.new_from_filename
+  end 
+  
+  
+  def self.create_from_filename
+  end 
+  
+  
+  def self.destroy_all
+  end 
+
+  
   def self.all
     @@all
   end
+  
 
   def save
     self.class.all << self
